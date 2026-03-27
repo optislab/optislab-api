cfdd903f870c434a39eebe4e0b6dcdfab49c4d72
OPTISLAB <optislab@gmail.com>
2026-03-27 15:02:59 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+// 서버 상태 확인
+app.get("/", (req, res) => {
+  res.send("API 살아있음");
+});
+
+// 문자 테스트 API
+app.post("/send-sms", async (req, res) => {
+  const { phone, message } = req.body;
+
+  console.log("문자 요청:", phone, message);
+
+  res.send({
+    success: true,
+    phone,
+    message
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`서버 실행됨: ${PORT}`);
+});
